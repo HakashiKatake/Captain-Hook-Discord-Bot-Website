8651cae7467c84ce60c15cb9be745c47dfd23971
SnowBit <dtshah24022006@gmail.com>
2021-08-11 20:54:13 +0530

    Added files

--- pages/commands.js
+++ pages/commands.js
@@ -1,14 +1,14 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import Link from 'next/link';
 
-export default function Home() {
+export default function Command() {
   return (
+    
     <div className={styles.container}>
       <Head>
         <title>Captain Hook - A fun Discord Bot</title>
-      <link rel="shortcut icon" href="/assets/logo.png" type="image/x-icon" />
+      {/* <link rel="shortcut icon" href="/assets/logo.png" type="image/x-icon" /> */}
       </Head>
       <header className={styles.header}>
         <h1 className={styles.title}>Captain Hook - Fun Discord Bot</h1>
@@ -21,20 +21,14 @@ export default function Home() {
         />
         {/* https://discord.com/api/oauth2/authorize?client_id=756715507306201130&permissions=8&scope=bot */}
         <p className={styles.desc}>
-          A cool discord bot for fun, memes, calculator, games, info, economy, utility, image and many more. <code>$help cmd</code> for commands<br/><br/>
-          One should give it a try to invite <b>Captain Hook</b> to your server.
+          Commands are as follows
         <ul>
           <li><a href="https://discord.com/api/oauth2/authorize?client_id=756715507306201130&permissions=8&scope=bot" target="_blank">It's <b>Captain Hook 🏴‍☠️</b> invite me now</a></li>
           <br/>
           <li><a href="https://discord.gg/9Ef8fq5sTA" target="_blank">Join our <b>Support</b> server</a></li>
         </ul>
-
-        {/* <Link href="/commands">
-          <a className="command">Commands</a>
-        </Link> */}
         </p>
       </header>
     </div>
-
   )
 }
